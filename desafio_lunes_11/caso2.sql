2eecb55f5998ef675f9fe978b655c83e7e276d54
Juan Luis Cabello <juan.luiscoropeza@gmail.com>
2021-01-11 22:49:43 -0300

    modificacion final

--- desafio_lunes_11/caso2.sql
+++ desafio_lunes_11/caso2.sql
@@ -5,7 +5,7 @@ CREATE TABLE curso(
 CREATE TABLE alumnos(
     rut VARCHAR(10) PRIMARY KEY,
     nombre VARCHAR(20) NOT NULL,
-    curso_id SERIAL,
+    curso_id INT,
     FOREIGN KEY (curso_id) REFERENCES curso(id)
 );
 CREATE TABLE pruebas(
@@ -13,7 +13,7 @@ CREATE TABLE pruebas(
     puntaje INT(2),
     alumnos_rut VARCHAR(10),
     FOREIGN KEY (alumnos_rut) REFERENCES alumnos(rut),
-    profesor_id SERIAL,
+    profesor_id INT,
     FOREIGN KEY (profesor_id) REFERENCES profesor(id)
 );
 CREATE TABLE profesor(
